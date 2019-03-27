ee0e17c6d8cafa694604c03dca87be879836f2f9
samor412 <you@example.com>
2019-03-27 20:17:11 +0100

    backPropagate

--- src/AI/backPropagate.java
+++ src/AI/backPropagate.java
@@ -1,11 +1,15 @@
 package ai;
+
 import java.util.ArrayList;
 import java.util.List;
 
 //import AI.selectionection
 
-public class BackPropagate{
-	public static void updateScore(MCSTreeNode node) {
-		
+public class BackPropagate {
+	public static void updateScore(MCSTreeNode node ,int n ,int r) {
+		MCSTreeNode currentNode = node;
+		while(currentNode.getParent() != null) {
+			currentNode.updateRNUCB(n, r);
+			currentNode = currentNode.getParent();
 	}
 }
