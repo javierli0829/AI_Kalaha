fdd95b321f6d02192673ff2c6de302b249f6089d
a2982601 <a2982601@gmail.com>
2019-03-27 19:37:49 +0100

    add functions

--- src/ai/main.java
+++ src/ai/main.java
@@ -1,7 +1,7 @@
 package ai;
 import java.lang.Math;
 
-public class AI {
+public class main {
   public static void main(String[] args) {
     StimulationGame g = new StimulationGame(new int[] { 7, 7, 7, 1, 7, 7, 7, 6, 6, 6, 1, 6, 6, 6 }, 1);
     randomPlayTest.randomPlay(g);
