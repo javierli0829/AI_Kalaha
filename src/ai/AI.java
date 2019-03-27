f7501a59f2eb3c46fd837434a30fe018c7e6360c
a2982601 <a2982601@gmail.com>
2019-03-27 20:56:31 +0100

    changes

--- src/ai/AI.java
+++ src/ai/AI.java
@@ -5,12 +5,13 @@ import java.util.List;
 
 public class AI {
   public List<MCSTreeNode> leafNodes = new ArrayList<>();
-  public int[] decisionNode;
+  public List<MCSTreeNode> decisionNode = new ArrayList<>();
   public MCSTreeNode rootNode;
-  public int[] currentState;
+  public int[] rootCurrentState;
 
   public AI(int[] currentState){ // Constructor
-
+    this.rootNode = new MCSTreeNode(7, currentState); // 7 means root
+    this.rootCurrentState = currentState;
   }
   
   public void runAI(){
@@ -20,7 +21,10 @@ public class AI {
   public void expandNode(MCSTreeNode node){
     for(int count = 0; count < 7; count++){
       if(count == 3) continue;
+      // run one step and get currentState
       node.addChild(count, new MCSTreeNode(count, node.currentState));
+      // random
+      // update
     }
   }
 }
